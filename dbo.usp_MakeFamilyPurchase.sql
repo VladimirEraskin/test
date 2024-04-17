8489efd01a997c5450f0f2db2cfa42244ea58cc7
VladimirEraskin <166914923+VladimirEraskin@users.noreply.github.com>
2024-04-17 20:02:11 +0300

    Add files via upload

---
+++ dbo.usp_MakeFamilyPurchase.sql
@@ -0,0 +1,28 @@
+﻿create or alter procedure dbo.usp_MakeFamilyPurchase
+	@FamilySurName varchar(255)
+as
+begin
+	declare @ErrorMessage varchar(4000)
+		,@FamilyPurchase decimal(18,2)
+
+	if not exists (
+		select f.SurName
+		from dbo.Family as f
+		where @FamilySurName = f.SurName
+	)
+	begin
+		set @ErrorMessage = 'Такой семьи не существует'
+		raiserror(@ErrorMessage, 1, 1)
+
+		return
+	end
+
+	select @FamilyPurchase = (sum(b.Value))
+	from dbo.Basket as b
+	where b.ID_Family = (select f.ID from dbo.Family as f where f.SurName = @FamilySurName)
+
+	update f
+	set BudgetValue = f.BudgetValue - @FamilyPurchase
+	from dbo.Family as f
+	where f.SurName = @FamilySurName
+end
